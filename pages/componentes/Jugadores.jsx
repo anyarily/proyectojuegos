8d1f146fc32d1c7633bbec9a6a3d91618f1bf21d
redweb <michaelaybar@gmail.com>
2025-08-05 01:10:32 -0400

    mine

--- pages/componentes/Jugadores.jsx
+++ pages/componentes/Jugadores.jsx
@@ -68,6 +68,8 @@ function edicionfinal(){
 
 
     return<div>
+   
+   
     <div className={styles.general} ><input onChange={(e)=>{setagregando(e.target.value)}} value={agregando} ></input>
          {cambioBonton? <button onClick={agregar}>agregar</button>: <button onClick={edicionfinal}>editar</button> }</div>
      
