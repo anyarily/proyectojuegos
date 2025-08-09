ef7fa8013a441ea300a9aae292c11b658071bd6a
redweb <michaelaybar@gmail.com>
2025-08-09 01:33:02 -0400

    nombre de yamil

--- pages/componentes/Jugadores.jsx
+++ pages/componentes/Jugadores.jsx
@@ -9,6 +9,8 @@ let [agregando,setagregando]=useState(" ")
 let [agregados,setagregados]=useState([])
 let [cambioBonton,setcambioBoton]=useState(true)
 let [objetoo,setobjetoo]=useState({})
+let [segundo,setsegundo]=useState(0)
+let [inputseg,setinputseg]=useState("")
 
 function agregar(){
 
@@ -62,12 +64,36 @@ function edicionfinal(){
 
 
 
+function numero(){
+
+
+
+let parseado= parseInt(inputseg)
+
+let sumadera= segundo + parseado
+
+setsegundo(sumadera )
+
+
+setinputseg(" ")
+
+
+
+}
+if(segundo >= 200  ){
+
+alert( " ya hay un ganador")
+setsegundo(0)
+}
 
 
 
 
 
     return<div>
+        <h2>yamil aybar </h2>
+        <input onChange={ function(e){setinputseg (e.target.value)  }} placeholder="tanto"  value={inputseg}></input>
+        <button onClick={function(){ numero()  }}>puntos</button>
         <h3>titulo del juego</h3>
     <div className={styles.general} ><input onChange={(e)=>{setagregando(e.target.value)}} value={agregando} ></input>
     {cambioBonton? <button onClick={agregar}>agregar</button>: <button onClick={edicionfinal}>editar</button> }</div>
@@ -84,6 +110,17 @@ function edicionfinal(){
       </div>         }    )}</div>
 
        
+       <h2>{segundo}</h2>
+
+
+
+
+
+
+
+
+
+
     </div>
 
 
